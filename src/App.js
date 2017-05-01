45b0b98fae6ceee343d66ca616281c1697ce3b66
Mitch <iammitchellreeves@gmail.com>
2017-05-01 19:36:51 +1000

    Implemented basic note app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      notes: ['My Balls', 'My Gooch'],
+      newNote: 'My Dick',
+    }
+  }
+
+  updateNewNote(event) {
+    this.setState({ newNote: event.target.value })
+  }
+
+  submitForm(e) {
+    e.preventDefault()
+
+    // Put information somewhere and reset new note
+    let notes = this.state.notes
+    let newNote = this.state.newNote
+    this.setState({ notes: notes.concat(newNote), newNote: '' })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>myRemembers</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You have no notes
         </p>
+        <div>
+          <form onSubmit={(e) => this.submitForm(e)}>
+            <input type="text"
+                   value={this.state.newNote}
+                   onChange={(event) => this.updateNewNote(event)}
+                   placeholder="What do you want to remember?"/>
+
+            <button>Add</button>
+          </form>
+
+          <ul>
+            {this.state.notes.map(
+              (note, index) => <li key={index}>{note}</li>
+            )}
+          </ul>
+        </div>
       </div>
-    );
+
+    )
   }
 }
 
-export default App;
+export default App
